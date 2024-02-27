5cb534972ee3eb21c91f3e389d96e0082f822775
Allan <allan3.huang@gmail.com>
2024-02-27 12:59:17 -0500

    set up an easy conversation chatbot with ChatGPT 3.5-turbo

---
+++ test_conversation_ai.py
@@ -0,0 +1,31 @@
+from openai import OpenAI
+client = OpenAI()
+import pprint
+
+messages=[
+    {"role": "system", "content": "You are a brilliant Michelin-starred level chef"},
+    {"role": "user", "content": "I am Allan. I want to make a lunch plan for my weekdays."},
+    {"role": "assistant", "content": "Hello, Allan. What kind of nutrition do you want to include?"},
+  ]
+
+
+def update_chat(messages, role, content):
+	messages.append({"role": role, "content": content})
+	return messages
+
+def get_response(messages):
+	completion = client.chat.completions.create(                                           
+          model="gpt-3.5-turbo",                                                                     
+          temperature = 0.2,
+          messages= messages,
+        ) 
+
+	return completion.choices[0].message.content
+
+while True:
+	pprint.pprint(messages)
+	user_input = input()
+	messages = update_chat(messages, "user", user_input)
+	model_response = get_response(messages)
+	messages = update_chat(messages, "assistant", model_response)
+#asyncio.run(chat_func())
